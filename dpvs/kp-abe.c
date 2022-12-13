3406c0f60d31e0c028a71b835cf9b4e2e438a828
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2022-12-14 00:04:32 +0100

    Add functions to init and clear public key and master secret key

--- dpvs/kp-abe.c
+++ dpvs/kp-abe.c
@@ -1,2 +1,46 @@
 #include "kp-abe.h"
 
+bool ABE_pub_key_new(ABE_pub_key_t pk)
+{
+  if (dpvs_init_base_vect(pk->d1, ND) && dpvs_init_base_vect(pk->d3, ND) &&
+      dpvs_init_base_vect(pk->g1, NG) && dpvs_init_base_vect(pk->g2, NG) &&
+      dpvs_init_base_vect(pk->f1, NF) && dpvs_init_base_vect(pk->f2, NF) &&
+      dpvs_init_base_vect(pk->f3, NF) && dpvs_init_base_vect(pk->h1, NH) &&
+      dpvs_init_base_vect(pk->h2, NH) && dpvs_init_base_vect(pk->h3, NH))
+  {
+    return true;
+  }
+
+  return false;
+}
+
+bool ABE_ms_key_new(ABE_ms_key_t msk)
+{
+  if (dpvs_init_dual_base_vect(msk->d1, ND) && dpvs_init_dual_base_vect(msk->d3, ND) &&
+      dpvs_init_dual_base_vect(msk->g1, NG) && dpvs_init_dual_base_vect(msk->g2, NG) &&
+      dpvs_init_dual_base_vect(msk->f1, NF) && dpvs_init_dual_base_vect(msk->f2, NF) &&
+      dpvs_init_dual_base_vect(msk->f3, NF) && dpvs_init_dual_base_vect(msk->h1, NH) &&
+      dpvs_init_dual_base_vect(msk->h2, NH) && dpvs_init_dual_base_vect(msk->h3, NH))
+  {
+    return true;
+  }
+
+  return false;
+}
+void ABE_free_pub_key(ABE_pub_key_t pk)
+{
+  dpvs_clear_base_vect(pk->d1); dpvs_clear_base_vect(pk->d3);
+  dpvs_clear_base_vect(pk->g1); dpvs_clear_base_vect(pk->g2);
+  dpvs_clear_base_vect(pk->f1); dpvs_clear_base_vect(pk->f2);
+  dpvs_clear_base_vect(pk->f3); dpvs_clear_base_vect(pk->h1);
+  dpvs_clear_base_vect(pk->h2); dpvs_clear_base_vect(pk->h3);
+}
+
+void ABE_free_ms_key(ABE_ms_key_t msk)
+{
+  dpvs_clear_dual_base_vect(msk->d1); dpvs_clear_dual_base_vect(msk->d3);
+  dpvs_clear_dual_base_vect(msk->g1); dpvs_clear_dual_base_vect(msk->g2);
+  dpvs_clear_dual_base_vect(msk->f1); dpvs_clear_dual_base_vect(msk->f2);
+  dpvs_clear_dual_base_vect(msk->f3); dpvs_clear_dual_base_vect(msk->h1);
+  dpvs_clear_dual_base_vect(msk->h2); dpvs_clear_dual_base_vect(msk->h3);
+}
--- dpvs/kp-abe.h
+++ dpvs/kp-abe.h
@@ -3,6 +3,11 @@
 
 #include "dpvs.h"
 
+#define ND  3
+#define NG  4
+#define NF  6
+#define NH  8
+
 typedef struct
 {
   g1_vect_t d1, d3;
@@ -23,6 +28,10 @@ typedef ABE_pub_key_st ABE_pub_key_t[1];
 typedef ABE_master_key_st ABE_ms_key_t[1];
 
 
+bool ABE_pub_key_new(ABE_pub_key_t pk);
+bool ABE_ms_key_new(ABE_ms_key_t msk);
 
+void ABE_free_pub_key(ABE_pub_key_t pk);
+void ABE_free_ms_key(ABE_ms_key_t msk);
 
 #endif
