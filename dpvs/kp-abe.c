14bd1335d4fd1543f71308535a018659983dbcd9
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2022-12-14 00:10:16 +0100

    Function corresponding to the setup algorithm. Set thes public params (public key) and master secret key.

--- dpvs/kp-abe.c
+++ dpvs/kp-abe.c
@@ -27,6 +27,61 @@ bool ABE_ms_key_new(ABE_ms_key_t msk)
 
   return false;
 }
+
+bool ABE_setup(ABE_pub_key_t pk, ABE_ms_key_t msk)
+{
+  bool ret = true;
+  dpvs_t D, G, F, H;
+
+  if (ABE_pub_key_new(pk) && ABE_ms_key_new(msk)) {
+    if (dpvs_gen(D, ND) && dpvs_gen(G, NG) && dpvs_gen(F, NF) && dpvs_gen(H, NH))
+    {
+      /* Public key */
+      dpvs_g1_vect_copy(pk->d1, D->base[0]);
+      dpvs_g1_vect_copy(pk->d3, D->base[2]);
+
+      dpvs_g1_vect_copy(pk->g1, G->base[0]);
+      dpvs_g1_vect_copy(pk->g2, G->base[1]);
+
+      dpvs_g1_vect_copy(pk->f1, F->base[0]);
+      dpvs_g1_vect_copy(pk->f2, F->base[1]);
+      dpvs_g1_vect_copy(pk->f3, F->base[2]);
+
+      dpvs_g1_vect_copy(pk->h1, H->base[0]);
+      dpvs_g1_vect_copy(pk->h2, H->base[1]);
+      dpvs_g1_vect_copy(pk->h3, H->base[2]);
+
+      /* Master secret key */
+      dpvs_g2_vect_copy(msk->d1, D->dual_base[0]);
+      dpvs_g2_vect_copy(msk->d3, D->dual_base[2]);
+
+      dpvs_g2_vect_copy(msk->g1, G->dual_base[0]);
+      dpvs_g2_vect_copy(msk->g2, G->dual_base[1]);
+
+      dpvs_g2_vect_copy(msk->f1, F->dual_base[0]);
+      dpvs_g2_vect_copy(msk->f2, F->dual_base[1]);
+      dpvs_g2_vect_copy(msk->f3, F->dual_base[2]);
+
+      dpvs_g2_vect_copy(msk->h1, H->dual_base[0]);
+      dpvs_g2_vect_copy(msk->h2, H->dual_base[1]);
+      dpvs_g2_vect_copy(msk->h3, H->dual_base[2]);
+    }
+    else {
+      ret = _error_alloc_fail_();
+    }
+  }
+  else {
+    ret = _error_alloc_fail_();
+  }
+
+  dpvs_clear(D);
+  dpvs_clear(G);
+  dpvs_clear(F);
+  dpvs_clear(H);
+
+  return ret;
+}
+
 void ABE_free_pub_key(ABE_pub_key_t pk)
 {
   dpvs_clear_base_vect(pk->d1); dpvs_clear_base_vect(pk->d3);
--- dpvs/kp-abe.h
+++ dpvs/kp-abe.h
@@ -30,6 +30,7 @@ typedef ABE_master_key_st ABE_ms_key_t[1];
 
 bool ABE_pub_key_new(ABE_pub_key_t pk);
 bool ABE_ms_key_new(ABE_ms_key_t msk);
+bool ABE_setup(ABE_pub_key_t pk, ABE_ms_key_t msk);
 
 void ABE_free_pub_key(ABE_pub_key_t pk);
 void ABE_free_ms_key(ABE_ms_key_t msk);
