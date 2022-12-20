822f5bf09c4a71589bf68d125fd27400ceb20600
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2022-12-20 21:21:39 +0100

    Little change in test functions

--- dpvs/test_main.c
+++ dpvs/test_main.c
@@ -2,7 +2,8 @@
  *     gcc *.c -g -Wall -lgmp /usr/lib/librelic_s.a -I/usr/include/relic     *
  *****************************************************************************/
 
-#include "kp-abe.h"
+#include <assert.h>
+#include "test.h"
 
 /* Declaration of external variables, there are declared in the headers files */
 group_setting_t params;
@@ -11,17 +12,19 @@ bn_t Fq;
 
 int main(int argc, char const *argv[])
 {
+  int dim;
+
+  if (argc != 2 || (dim = atoi(argv[1])) < 0) {
+    fprintf(stderr, "Please enter the dimension (positive integer) of matrix\n");
+    return -1;
+  }
+
   core_init();
   pc_param_set_any();
   set_params();
 
-  ABE_pub_key_t pk;
-  ABE_ms_key_t msk;
-
-  ABE_setup(pk, msk);
-
-  ABE_free_pub_key(pk);
-  ABE_free_ms_key(msk);
+  printf("[*] Cheking DPVS_dual_bases...\n");
+  assert(test_DPVS_dual_bases(dim) == true);
 
   clear_params();
   core_clean();
