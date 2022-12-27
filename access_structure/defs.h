4d7bcb4f52f633c1852c470052c6d81844441256
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2022-12-27 02:35:53 +0100

    Add test files for access structure

---
+++ access_structure/defs.h
@@ -0,0 +1,26 @@
+
+#ifndef __DEFS_H__
+#define __DEFS_H__
+
+#include <assert.h>
+#include <sched.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include "legacy/zattributelist.h"
+#include "legacy/zconstants.h"
+#include "legacy/zinteger.h"
+#include "legacy/zpolicy.h"
+#include "legacy/zbytestring.h"
+#include "legacy/zfunctioninput.h"
+#include "legacy/zobject.h"
+
+#include "legacy/arith/zgroup.h"
+#include "legacy/arith/zelement_bp.h"
+#include "legacy/lsss/zlsss.h"
+
+extern "C" {
+  #include "legacy/arith/zelement.h"
+}
+
+#endif /* ifdef __DEFS_H__ */
