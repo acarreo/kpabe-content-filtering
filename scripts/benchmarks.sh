3a3bdcb47573baa6ed75a46b38aa596ee08f0db2
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2023-05-16 12:11:55 +0200

    Fix volume mount path

--- scripts/benchmarks.sh
+++ scripts/benchmarks.sh
@@ -5,7 +5,7 @@ if [ "$curve" != "bls12-446" ]; then
     curve="bls12-381"
 fi
 
-docker run -id --name benchmark --rm -v /home/adam/these/depots/thesis/:/root presto-content-filtering-use-case:relic-0.6.0--lsss
+docker run -id --name benchmark --rm -v $PWD/:/root presto-content-filtering-use-case:relic-0.6.0--lsss
 
 echo "Compiltation"
 docker exec -ti benchmark make clean CURVE=$curve
