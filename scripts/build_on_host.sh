42e9bfcd263bb169f76704442d5c52c1149aed31
Adam Oumar Abdel-rahman <47461147+acarreo@users.noreply.github.com>
2023-07-27 18:23:54 +0000

    Add new script to build on host

---
+++ scripts/build_on_host.sh
@@ -0,0 +1,23 @@
+export RELIC_TAG="0.6.0"
+export CURVE="bls12-381"
+export RELIC_INCLUDE="/usr/local/include/relic"
+WORKDIR=$(pwd)
+
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends cmake make g++ ca-certificates libgmp-dev flex libfl-dev pkg-config
+
+# Build RELIC
+git clone https://github.com/relic-toolkit/relic /tmp/relic
+cd /tmp/relic
+#git switch --detach ${RELIC_TAG}
+mkdir "target_${CURVE}"
+cd "target_${CURVE}"
+../preset/x64-pbc-${CURVE}.sh ..
+make -j
+sudo make install
+
+# Build LSSS
+cd $WORKDIR
+make -j -C ../lsss
+sudo mv ../lsss/liblsss.a /usr/local/lib/
+make -C ../lsss clean
