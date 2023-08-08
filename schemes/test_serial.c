873d13e985147b0090817f2bfba245aabbcae232
Adam Oumar Abdel-rahman <47461147+acarreo@users.noreply.github.com>
2023-08-08 08:22:17 +0000

    Pending changes exported from your codespace

--- schemes/test_serial.c
+++ schemes/test_serial.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <msgpack.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "../dpvs/dpvs.h"
 #include "../utils.h"
@@ -9,6 +10,7 @@
 group_setting_t params;
 bn_t Fq;
 
+#define G1_SIZE_BIN     (RLC_PC_BYTES * 2 + 1)
 
 void random_vect_g1 (G1_VS_VECT vect) {
   if (vect && vect->coord) {
@@ -18,71 +20,122 @@ void random_vect_g1 (G1_VS_VECT vect) {
   }
 }
 
-void print_serialized_buffer(const msgpack_sbuffer* sbuf) {
-  for (size_t i = 0; i < sbuf->size; i++) {
-    printf("%02X",sbuf->data[i] & 0xFF);
+// compare two g1_vect_t
+bool compare_g1_vect(const G1_VS_VECT vect1, const G1_VS_VECT vect2) {
+  if (vect1->dim != vect2->dim) {
+    return false;
   }
-  printf("\n");
+
+  for (uint8_t i = 0; i < vect1->dim; i++) {
+    if (g1_cmp(vect1->coord[i], vect2->coord[i]) != RLC_EQ) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-void serialize_g1_vect(const G1_VS_VECT vect, msgpack_sbuffer* sbuf) {
-  msgpack_packer packer;
-  msgpack_packer_init(&packer, sbuf, msgpack_sbuffer_write);
+void print_hexdump(const void *buffer, size_t size) {
+  const uint8_t *data = (const uint8_t *)buffer;
 
-  msgpack_pack_array(&packer, 2);
-  msgpack_pack_uint8(&packer, vect->dim);
+  for (size_t i = 0; i < size; i += 16) {
+    printf("%08zx: ", i);
 
-  msgpack_pack_array(&packer, vect->dim);
-  for (int i = 0; i < vect->dim; i++) {
-    int size = g1_size_bin(vect->coord[i], 0);
-    uint8_t* buf_g1 = (uint8_t*) malloc(size);
+    for (size_t j = 0; j < 16; j++) {
+      if (i + j < size) {
+        printf("%02x ", data[i + j]);
+      } else {
+        printf("   ");
+      }
 
-    g1_write_bin(buf_g1, size, vect->coord[i], 0);
-    msgpack_pack_v4raw(&packer, size);
-    msgpack_pack_v4raw_body(&packer, buf_g1, size);
+      if (j == 7) {
+        printf(" ");
+      }
+    }
 
-    free(buf_g1);
+    printf("| ");
+
+    for (size_t j = 0; j < 16; j++) {
+      if (i + j < size) {
+        char c = data[i + j];
+        if (c >= 32 && c <= 126) {
+          printf("%c", c);
+        } else {
+          printf(".");
+        }
+      } else {
+        printf(" ");
+      }
+    }
+
+    printf("\n");
   }
 }
 
-// deserialize a G1_VS_VECT from a msgpack_sbuffer
-#if 0
-G1_VS_VECT deserialize_g1_vect(const msgpack_sbuffer* sbuf) {
-  G1_VS_VECT vect = NULL;
+void serialize_g1_vector(const G1_VS_VECT vect, uint8_t** buf, int* size) {
 
-  msgpack_unpacker unpacker;
-  msgpack_unpacker_init(&unpacker, MSGPACK_UNPACKER_INIT_BUFFER_SIZE);
+  if (vect && vect->coord) {
+    int shift = 0;
+    uint8_t *buf_tmp = (uint8_t*) malloc((vect->dim) * (G1_SIZE_BIN + sizeof(uint8_t)));
 
-  // Feed the serialized buffer to the unpacker
-  msgpack_unpacker_reserve_buffer(&unpacker, sbuf->size);
-  memcpy(msgpack_unpacker_buffer(&unpacker), sbuf->data, sbuf->size);
-  msgpack_unpacker_buffer_consumed(&unpacker, sbuf->size);
+    if (buf_tmp == NULL) {
+      fprintf(stderr, "Error in serialize_g1_vector: malloc failed\n");
+      exit(EXIT_FAILURE);
+    }
 
-  // Unpack the serialized data
-  msgpack_unpacked deserialized_obj;
-  msgpack_unpack_return ret = msgpack_unpacker_next(&unpacker, &deserialized_obj, NULL);
+    for (uint8_t i = 0; i < vect->dim; i++) {
+      uint8_t g1_size = g1_size_bin(vect->coord[i], 0);
+      memcpy(buf_tmp + shift, &g1_size, sizeof(uint8_t));
+      g1_write_bin(buf_tmp + shift + sizeof(uint8_t), g1_size, vect->coord[i], 0);
+      shift += g1_size + sizeof(uint8_t);
+    }
 
+    *size = sizeof(int) + sizeof(uint8_t) + shift;
+    *buf = (uint8_t*) malloc(*size);
+    if (buf == NULL) {
+      fprintf(stderr, "Error in serialize_g1_vector: malloc failed\n");
+      free(buf_tmp);
+      exit(EXIT_FAILURE);
+    }
 
+    memcpy(*buf, &shift, sizeof(int));   // write size of buffer representing the g1 vector
+    memcpy(*buf + sizeof(int), &vect->dim, sizeof(uint8_t));  // write dimension of g1 vector
+    memcpy(*buf + sizeof(int) + sizeof(uint8_t), buf_tmp, shift);  // write g1 vector
 
-  // G1_VS_VECT vect = dpvs_create_g1_vect(dim);
-  // if (!vect) {
-  //   fprintf(stderr, "Error unpacking the serialized buffer: unable to create the G1_VS_VECT.\n");
-  //   return NULL;
-  // }
-
-  // for (int i = 0; i < dim; i++) {
-  //   deserialized_obj = msgpack_unpack_next(&unpacker, &deserialized_obj);
-  //   if (deserialized_obj.type != MSGPACK_OBJECT_RAW) {
-  //     fprintf(stderr, "Error unpacking the serialized buffer: the deserialized object is not a raw object.\n");
-  //     dpvs_clear_g1_vect(vect);
-  //     return NULL;
-  //   }
-  //   g1_read_bin(vect->coord[i], deserialized_obj.via.raw.ptr, deserialized_obj.via.raw.size);
-  // }
-
-  return vect;
+    free(buf_tmp);
+  }
 }
-#endif
+
+void deserialize_g1_vector(const uint8_t* buf, const int size, g1_vect_t** deserialized_vector) {
+  int buf_size = 0;
+  int shift = 0;
+  uint8_t dim = 0;
+  if (buf != NULL && memcpy(&buf_size, buf, sizeof(int)) && buf_size > 0) {
+    memcpy(&dim, buf + sizeof(int), sizeof(uint8_t));
+    shift = sizeof(int) + sizeof(uint8_t);
+
+    *deserialized_vector = dpvs_create_g1_vect(dim);
+    G1_VS_VECT vect = *deserialized_vector;
+    if (vect != NULL) {
+      for (uint8_t i = 0; i < dim; i++) {
+        uint8_t g1_size = 0;
+        memcpy(&g1_size, buf + shift, sizeof(uint8_t));
+        g1_read_bin(vect->coord[i], buf + shift + sizeof(uint8_t), g1_size);
+        shift += g1_size + sizeof(uint8_t);
+      }
+
+      if (shift != size || (shift - sizeof(int) - sizeof(uint8_t)) != buf_size) {
+        fprintf(stderr, "Error deserializing the g1 vector: the size of the buffer is not correct.\n");
+        dpvs_clear_g1_vect(vect);
+        return;
+      }
+    }
+    else {
+      fprintf(stderr, "Error deserializing the g1 vector: dpvs_create_g1_vect failed.\n");
+      return;
+    }
+  }
+}
 
 
 int main(int argc, char const *argv[])
@@ -92,23 +145,43 @@ int main(int argc, char const *argv[])
     return 1;
   }
 
-  G1_VS_VECT vect = dpvs_create_g1_vect(2);
+  // test serialization of a G1_VS_VECT
+  G1_VS_VECT vect = dpvs_create_g1_vect(5);
+  if (!vect) {
+    printf("Error creating the G1_VS_VECT.\n");
+    clean_libraries();
+    return 1;
+  }
+
   random_vect_g1(vect);
-  print_vect_base(vect);
 
-  // msgpack_sbuffer sbuf;
-  // msgpack_sbuffer_init(&sbuf);
+  uint8_t *buf = NULL;
+  int size = 0;
+  serialize_g1_vector(vect, &buf, &size);
+  print_hexdump(buf, size);
 
-  // serialize_g1_vect(vect, &sbuf);
-  // print_serialized_buffer(&sbuf);
+  G1_VS_VECT vect2;
+  deserialize_g1_vector(buf, size, &vect2);
 
-  // G1_VS_VECT vect_deserialized = deserialize_g1_vect(&sbuf);
 
-  // print_vect_base(vect_deserialized);
+  if (!vect2) {
+    printf("Error deserializing the G1_VS_VECT.\n");
+    dpvs_clear_g1_vect(vect);
+    clean_libraries();
+    return 1;
+  }
 
-  // msgpack_sbuffer_destroy(&sbuf);
+  // check that the two vectors are equal
+  if (compare_g1_vect(vect, vect2) == true) {
+    printf("Serialization and deserialization of a G1_VS_VECT successful.\n");
+    printf("Size of the serialized buffer: %d\n", size);
+  } else {
+    printf("Serialization and deserialization of a G1_VS_VECT failed.\n");
+  }
+
+  // clean vectors
   dpvs_clear_g1_vect(vect);
-  // dpvs_clear_g1_vect(vect_deserialized);
+  dpvs_clear_g1_vect(vect2);
   clean_libraries();
 
   return 0;
