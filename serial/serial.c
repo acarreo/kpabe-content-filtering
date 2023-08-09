88046230ca5fc8194203972ff24f18921d47f5b7
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2023-08-09 11:40:01 +0200

    Add serialization functions for G2 elements G2 vectors.

--- serial/serial.c
+++ serial/serial.c
@@ -47,6 +47,42 @@ int deserialize_g1_element(const uint8_t buf[], g1_t g1) {
 	return (sizeof(uint8_t) + size);
 }
 
+/**
+ * @brief Serialize a G2 element : g2_t element
+ *
+ * @param g2 the element to serialize
+ * @param buf buffer conatins serialization of g2; it must be allocated with
+ *            the right size : G2_SIZE_BIN + sizeof(uint8_t)
+ * @return size of the serialized element in bytes or 0 if errors occured
+ */
+int serialize_g2_element(const g2_t g2, uint8_t buf[]) {
+
+  if (buf == NULL) return 0;
+
+  uint8_t size = g2_size_bin(g2, 0);
+  memcpy(buf, &size, sizeof(uint8_t));
+  g2_write_bin(buf + sizeof(uint8_t), size, g2, 0);
+
+  return (sizeof(uint8_t) + size);
+}
+
+/**
+ * @brief Deserialize a G2 element : g2_t element
+ *
+ * @param buf the buffer to deserialize in g2
+ * @param g2 the result of the deserialization of buf, must be allocated before
+ *           calling the function
+ * @return size of the serialized element in bytes or 0 if errors occured
+ */
+int deserialize_g2_element(const uint8_t buf[], g2_t g2) {
+
+  if (buf == NULL) return 0;
+
+  uint8_t size = 0;
+  memcpy(&size, buf, sizeof(uint8_t));
+  g2_read_bin(g2, buf + sizeof(uint8_t), size);
+
+  return (sizeof(uint8_t) + size);
 }
 
 /**
@@ -162,6 +198,125 @@ int deserialize_g1_vector(const uint8_t* buf, const int size, g1_vect_t** dest_v
   }
   else {
     fprintf(stderr, "Error deserializing the g1 vector: the buffer is NULL or the size is not correct.\n");
+    ret++;
+  }
+
+  return ret;
+}
+
+/**
+ * @brief Serialize a vector of G2 elements : G2_VS_VECT element
+ *
+ * @param vect  the vector to serialize
+ * @param buf_serialized buffer to store the serialized vector; *buf is allocated inside the function
+ * @param size size of the serialized vector in bytes
+ * @return 0 if success, an error code otherwise
+ */
+int serialize_g2_vector(const G2_VS_VECT vect, uint8_t **buf_serialized, int *size)
+{
+  int shift = 0;
+  int g2_size = 0;
+  int total_size = 0;
+  uint8_t *buf_tmp = NULL;
+
+  if (vect && vect->coord) {
+
+    // Calculate total size needed for serialization
+    total_size = vect->dim;
+    for (uint8_t i = 0; i < vect->dim; i++)
+      total_size += g2_size_bin(vect->coord[i], 0);
+
+    if ((buf_tmp = (uint8_t*) malloc(total_size)) == NULL) {
+      fprintf(stderr, "Error in serialize_g2_vector: malloc failed\n");
+      return 1;
+    }
+
+    for (uint8_t i = 0; i < vect->dim; i++) {
+      if ((g2_size = serialize_g2_element(vect->coord[i], buf_tmp + shift)) == 0) {
+        fprintf(stderr, "Error in serialize_g2_vector: serialize_g2_element failed\n");
+        free(buf_tmp);
+        return 2;
+      }
+      shift += g2_size;
+    }
+
+    // At this point, shift is the size of the buffer representing the g2 vector
+    // shift must be egal to total_size
+    if (shift != total_size) {
+      fprintf(stderr, "Error in serialize_g2_vector: shift != total_size\n");
+      free(buf_tmp);
+      return 3;
+    }
+
+    total_size += sizeof(int) + sizeof(uint8_t);
+    if ((*buf_serialized = (uint8_t*) malloc(total_size)) == NULL) {
+      fprintf(stderr, "Error in serialize_g2_vector: malloc failed\n");
+      free(buf_tmp);
+      return 4;
+    }
+
+    // write size of buffer representing the g2 vector, dim of g2 vector and g2 vector
+    memcpy(*buf_serialized, &shift, sizeof(int));
+    memcpy(*buf_serialized + sizeof(int), &vect->dim, sizeof(uint8_t));
+    memcpy(*buf_serialized + sizeof(int) + sizeof(uint8_t), buf_tmp, shift);
+    *size = total_size;
+
+    free(buf_tmp);
+  }
+  else {
+    fprintf(stderr, "Error in serialize_g2_vector: the g2 vector is NULL\n");
+    return 5;
+  }
+
+  return 0;
+}
+
+/**
+ * @brief Deserialize a vector of G2 elements : G2_VS_VECT element
+ *
+ * @param buf buffer to deserialize
+ * @param size buffer size
+ * @param dest_vect result of the deserialization of buf; *dest_vect is allocated inside the function
+ * @return 0 if success, an error code otherwise
+ */
+int deserialize_g2_vector(const uint8_t* buf, const int size, g2_vect_t** dest_vect) {
+  int ret = 0;
+  int buf_size = 0;
+  int g2_size = 0;
+  int shift = 0;
+  uint8_t dim = 0;
+
+  if (buf != NULL && memcpy(&buf_size, buf, sizeof(int)) && buf_size > 0) {
+    memcpy(&dim, buf + sizeof(int), sizeof(uint8_t));
+    shift = sizeof(int) + sizeof(uint8_t);
+
+    *dest_vect = dpvs_create_g2_vect(dim);
+    G2_VS_VECT vect = *dest_vect;
+    if (vect != NULL) {
+      for (uint8_t i = 0; i < dim; i++) {
+        g2_size = deserialize_g2_element(buf + shift, vect->coord[i]);
+        if (g2_size == 0) {
+          fprintf(stderr, "Error deserializing the g2 vector: deserialize_g2_element failed.\n");
+          dpvs_clear_g2_vect(vect);
+          ret++;
+          break;
+        }
+        shift += g2_size;
+      }
+
+      if (shift != size || (shift - sizeof(int) - sizeof(uint8_t)) != buf_size) {
+        fprintf(stderr, "Error deserializing the g2 vector: the size of the buffer is not correct.\n");
+        dpvs_clear_g2_vect(vect);
+        ret++;
+      }
+    }
+    else {
+      fprintf(stderr, "Error deserializing the g2 vector: dpvs_create_g2_vect failed.\n");
+      ret++;
+    }
+  }
+  else {
+    fprintf(stderr, "Error deserializing the g2 vector: the buffer is NULL or the size is not correct.\n");
     ret++;
   }
 
--- serial/serial.h
+++ serial/serial.h
@@ -13,14 +13,19 @@
 #include "../utils.h"
 
 #define G1_SIZE_BIN     (RLC_PC_BYTES * 2 + 1)
+#define G2_SIZE_BIN     (RLC_PC_BYTES * 4 + 1)
 
 
 int serialize_g1_element(const g1_t g1, uint8_t buf[]);
 int deserialize_g1_element(const uint8_t buf[], g1_t g1);
 
+int serialize_g2_element(const g2_t g2, uint8_t buf[]);
+int deserialize_g2_element(const uint8_t buf[], g2_t g2);
+
 int serialize_g1_vector(const G1_VS_VECT vect, uint8_t** buf_serialized, int* size);
 int deserialize_g1_vector(const uint8_t* buf, const int size, g1_vect_t** deserialized_vector); 
 
-
+int serialize_g2_vector(const G2_VS_VECT vect, uint8_t** buf_serialized, int* size);
+int deserialize_g2_vector(const uint8_t* buf, const int size, g2_vect_t** deserialized_vector);
 
 #endif // __SERIAL_H__
