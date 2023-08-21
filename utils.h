0855dde94e6e6953900062a4e7f091415eab9e3f
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2023-08-21 15:51:23 +0200

    Introduce a function to derive a symmetric key from a provided seed (`bn_t`).

--- utils.h
+++ utils.h
@@ -9,6 +9,7 @@ void clean_libraries(void);
 
 void str_to_bn(bn_t hash, const char *digest, int len, bn_t order);
 void gt_md_map(uint8_t* hash, gt_t gt);
+void derive_session_key(uint8_t* session_key, bn_t sk);
 void print_vect_base(const G1_VS_VECT vect);
 void print_vect_dual_base(const G2_VS_VECT dvect);
 
