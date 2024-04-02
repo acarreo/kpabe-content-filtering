e2df4029a3213e144668e9e4093d1a7a2dee279b
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2024-04-02 09:59:50 +0200

    Implement two functions to benchmark serialization of public and master keys

--- bench/new_bench.cpp
+++ bench/new_bench.cpp
@@ -23,6 +23,9 @@ int main(int argc, char **argv)
   if (!init_libraries()) return 2;
 
   bench_serialization_params();
+  bench_serialization_params_iostream();
+  bench_serialization_params(BENCH_ROUNDS, BIN_UNCOMPRESSED);
+  bench_serialization_params_iostream(BENCH_ROUNDS, BIN_UNCOMPRESSED);
   // bench_serialization_dec_key();
   // bench_encryption();
   // bench_decryption();
@@ -32,8 +35,11 @@ int main(int argc, char **argv)
   return 0;
 }
 
-void bench_serialization_params(int rounds)
+void bench_serialization_params(int rounds, CompressionType compress)
 {
+  cout << "\n----------------> START : " << __func__ << endl;
+  cout << "Benchmarking serialization of public and master keys -- Compression = " << compress << "\n" << endl;
+
   KPABE_DPVS kpabe;
   if (!kpabe.setup()) {
     std::cerr << "Error: Could not setup keys" << std::endl;
@@ -43,31 +49,112 @@ void bench_serialization_params(int rounds)
   KPABE_DPVS_PUBLIC_KEY public_key = kpabe.get_public_key();
   KPABE_DPVS_MASTER_KEY master_key = kpabe.get_master_key();
 
-  cout << "\nBenchmarking serialization of public and master keys\n" << endl;
 
   // serialization of public key
-  duration<double, std::micro> duration_pk(0);
+  duration<double, std::micro> ser_pk_duration(0);
+  duration<double, std::micro> ser_mk_duration(0);
+  duration<double, std::micro> des_pk_duration(0);
+  duration<double, std::micro> des_mk_duration(0);
+
+  chrono::time_point<chrono::high_resolution_clock> t1, t2, t3;
+
   for (int i = 0; i < rounds; i++) {
     ByteString public_key_bytes;
-  
-    auto t1 = high_resolution_clock::now();
-    public_key.serialize(public_key_bytes);
-    auto t2 = high_resolution_clock::now();
-
-    duration_pk += duration_cast<microseconds>( t2 - t1 );
-  }
-  cout << "Serialization of public key --> ByteString: " << duration_pk.count() / rounds << " us" << endl;
-
-  // serialization of master key
-  duration<double, std::micro> duration_mk(0);
-  for (int i = 0; i < rounds; i++) {
     ByteString master_key_bytes;
-  
-    auto t1 = high_resolution_clock::now();
-    master_key.serialize(master_key_bytes);
-    auto t2 = high_resolution_clock::now();
 
-    duration_mk += duration_cast<microseconds>( t2 - t1 );
+    KPABE_DPVS_PUBLIC_KEY public_key2;
+    KPABE_DPVS_MASTER_KEY master_key2;
+
+    t1 = high_resolution_clock::now();
+    public_key.serialize(public_key_bytes, compress);
+    t2 = high_resolution_clock::now();
+    public_key2.deserialize(public_key_bytes);
+    t3 = high_resolution_clock::now();
+
+    ser_pk_duration += duration_cast<microseconds>( t2 - t1 );
+    des_pk_duration += duration_cast<microseconds>( t3 - t2 );
+  
+    t1 = high_resolution_clock::now();
+    master_key.serialize(master_key_bytes, compress);
+    t2 = high_resolution_clock::now();
+    master_key2.deserialize(master_key_bytes);
+    t3 = high_resolution_clock::now();
+
+    ser_mk_duration += duration_cast<microseconds>( t2 - t1 );
+    des_mk_duration += duration_cast<microseconds>( t3 - t2 );
+
+    if (public_key != public_key2 || master_key != master_key2) {
+      std::cerr << "Error: Public and master keys are not equal" << std::endl;
+      return;
+    }
   }
-  cout << "Serialization of master key --> ByteString: " << duration_mk.count() / rounds << " us" << endl;
+  cout << "Serialization of public key --> ByteString: " << (int) ser_pk_duration.count() / rounds << " us" << endl;
+  cout << "Serialization of master key --> ByteString: " << (int) ser_mk_duration.count() / rounds << " us" << endl;
+  cout << "-----------------------------------------------------------------------" << endl;
+  cout << "Deserialization of public key <-- ByteString: " << (int) des_pk_duration.count() / rounds << " us" << endl;
+  cout << "Deserialization of master key <-- ByteString: " << (int) des_mk_duration.count() / rounds << " us" << endl;
+
+  cout << "Size of public key: " << public_key.getSizeInBytes(compress) << " bytes" << endl;
+  cout << "Size of master key: " << master_key.getSizeInBytes(compress) << " bytes" << endl;
+}
+
+
+void bench_serialization_params_iostream(int rounds, CompressionType compress)
+{
+  cout << "\n----------------> START : " << __func__ << endl;
+  cout << "Benchmarking serialization of public and master keys -- Compression = " << compress << "\n" << endl;
+
+  KPABE_DPVS kpabe;
+  if (!kpabe.setup()) {
+    std::cerr << "Error: Could not setup keys" << std::endl;
+    return;
+  }
+
+  KPABE_DPVS_PUBLIC_KEY public_key = kpabe.get_public_key();
+  KPABE_DPVS_MASTER_KEY master_key = kpabe.get_master_key();
+
+  duration<double, std::micro> ser_pk_duration(0);
+  duration<double, std::micro> ser_mk_duration(0);
+  duration<double, std::micro> des_pk_duration(0);
+  duration<double, std::micro> des_mk_duration(0);
+
+  chrono::time_point<chrono::high_resolution_clock> t1, t2, t3;
+
+  for (int i = 0; i < rounds; i++) {
+    stringstream ss_public_key, ss_master_key;
+
+    KPABE_DPVS_PUBLIC_KEY public_key2;
+    KPABE_DPVS_MASTER_KEY master_key2;
+
+    t1 = high_resolution_clock::now();
+    public_key.serialize(ss_public_key, compress);
+    t2 = high_resolution_clock::now();
+    public_key2.deserialize(ss_public_key);
+    t3 = high_resolution_clock::now();
+
+    ser_pk_duration += duration_cast<microseconds>( t2 - t1 );
+    des_pk_duration += duration_cast<microseconds>( t3 - t2 );
+
+    t1 = high_resolution_clock::now();
+    master_key.serialize(ss_master_key, compress);
+    t2 = high_resolution_clock::now();
+    master_key2.deserialize(ss_master_key);
+    t3 = high_resolution_clock::now();
+
+    ser_mk_duration += duration_cast<microseconds>( t2 - t1 );
+    des_mk_duration += duration_cast<microseconds>( t3 - t2 );
+
+    if (public_key != public_key2 || master_key != master_key2) {
+      std::cerr << "Error: Public and master keys are not equal" << std::endl;
+      return;
+    }
+  }
+  cout << "Serialization of public key --> iostream: " << (int) ser_pk_duration.count() / rounds << " us" << endl;
+  cout << "Serialization of master key --> iostream: " << (int) ser_mk_duration.count() / rounds << " us" << endl;
+  cout << "-----------------------------------------------------------------------" << endl;
+  cout << "Deserialization of public key <-- iostream: " << (int) des_pk_duration.count() / rounds << " us" << endl;
+  cout << "Deserialization of master key <-- iostream: " << (int) des_mk_duration.count() / rounds << " us" << endl;
+
+  cout << "Size of public key: " << public_key.getSizeInBytes(compress) << " bytes" << endl;
+  cout << "Size of master key: " << master_key.getSizeInBytes(compress) << " bytes" << endl;
 }
--- bench/new_bench.hpp
+++ bench/new_bench.hpp
@@ -11,10 +11,13 @@
 
 #include "../kpabe/kpabe.hpp"
 
-void bench_serialization_params(int rounds = 1000);     // public and master keys
-void bench_serialization_dec_key(int rounds = 1000);    // decryption key
-void bench_encryption(int rounds = 1000);
-void bench_decryption(int rounds = 1000);
+#define BENCH_ROUNDS 1000
+
+void bench_serialization_params(int rounds = BENCH_ROUNDS, CompressionType compress = BIN_COMPRESSED);
+void bench_serialization_params_iostream(int rounds = BENCH_ROUNDS, CompressionType compress = BIN_COMPRESSED);
+void bench_serialization_dec_key(int rounds = BENCH_ROUNDS);
+void bench_encryption(int rounds = BENCH_ROUNDS);
+void bench_decryption(int rounds = BENCH_ROUNDS);
 
 
 #endif // __NEW_BENCH_HPP__
