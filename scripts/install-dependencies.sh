3340bafc730a667e5b3b2c7fdb66b1a7fbfd5ce7
Adam Oumar Abdel-rahman <47461147+acarreo@users.noreply.github.com>
2024-08-28 23:06:51 +0000

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -3,18 +3,14 @@
 echo "Installing dependencies..."
 
 echo
-echo "Clone the ABE-LSSS..."
-git clone https://github.com/acarreo/abe-lsss.git /tmp/abe-lsss
-
-echo
 echo "Installing RELIC library..."
-cd /tmp/abe-lsss/compile/
-make
+cd ../abe-lsss/compile/
+make CURVE=bls12-381
 
 echo
 echo "Installing ABE-LSSS library..."
 cd ../
 mkdir -p build && cd build
-cmake ..
+cmake -DCOMPRESSION_ENABLED=ON ..
 make
 sudo make install
