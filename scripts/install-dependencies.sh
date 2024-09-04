996cf1f76c8dee11d1d3789e2cf8918f113dbc6d
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2024-09-04 11:38:45 +0200

    Update compilation scripts

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+ENABLE_COMPRESSION=${ENABLE_COMPRESSION:-OFF}
+CURVE=${CURVE:-bls12-381}
+
 echo "Installing dependencies..."
 
 echo
 echo "Installing RELIC library..."
 cd ../abe-lsss/compile/
-make CURVE=bls12-381
+make CURVE=${CURVE}
 
 echo
 echo "Installing ABE-LSSS library..."
 cd ../
 mkdir -p build && cd build
-cmake -DCOMPRESSION_ENABLED=ON ..
+cmake -DCOMPRESSION_ENABLED=${ENABLE_COMPRESSION} ..
 make
 sudo make install
