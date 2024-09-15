e49195c7a8556ff1442424d740194b2b1a511096
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2024-09-16 01:06:18 +0200

    Updated benchmark to serialize and deserialize decryption keys

--- bench/bench-serialization.cpp
+++ bench/bench-serialization.cpp
@@ -6,76 +6,6 @@
 
 using namespace std;
 
-static void BM_KPABE_DPVS_SerializePublicKey(benchmark::State& state) {
-  KPABE_DPVS kpabe;
-  if (!kpabe.setup()) {
-    cerr << "Error: Could not setup KPABE_DPVS" << endl;
-    exit(1);
-  }
-
-  auto pk = kpabe.get_public_key();
-  for (auto _ : state) {
-    OpenABEByteString pk_bytes;
-    pk.serialize(pk_bytes);
-  }
-}
-
-static void BM_KPABE_DPVS_DeserializePublicKey(benchmark::State& state) {
-  KPABE_DPVS kpabe;
-  if (!kpabe.setup()) {
-    cerr << "Error: Could not setup KPABE_DPVS" << endl;
-    exit(1);
-  }
-
-  OpenABEByteString pk_bytes;
-  kpabe.get_public_key().serialize(pk_bytes);
-  for (auto _ : state) {
-    KPABE_DPVS_PUBLIC_KEY pk_deserialized;
-    pk_deserialized.deserialize(pk_bytes);
-
-    // Check if deserialized public key is correct
-    if (!(pk_deserialized == kpabe.get_public_key())) {
-      cerr << "Error: Deserialized public key is incorrect" << endl;
-      exit(1);
-    }
-  }
-}
-
-static void BM_KPABE_DPVS_SerializeMasterKey(benchmark::State& state) {
-  KPABE_DPVS kpabe;
-  if (!kpabe.setup()) {
-    cerr << "Error: Could not setup KPABE_DPVS" << endl;
-    exit(1);
-  }
-
-  auto mk = kpabe.get_master_key();
-  for (auto _ : state) {
-    OpenABEByteString mk_bytes;
-    mk.serialize(mk_bytes);
-  }
-}
-
-static void BM_KPABE_DPVS_DeserializeMasterKey(benchmark::State& state) {
-  KPABE_DPVS kpabe;
-  if (!kpabe.setup()) {
-    cerr << "Error: Could not setup KPABE_DPVS" << endl;
-    exit(1);
-  }
-
-  OpenABEByteString mk_bytes;
-  kpabe.get_master_key().serialize(mk_bytes);
-  for (auto _ : state) {
-    KPABE_DPVS_MASTER_KEY mk_deserialized;
-    mk_deserialized.deserialize(mk_bytes);
-
-    // Check if deserialized master key is correct
-    if (!(mk_deserialized == kpabe.get_master_key())) {
-      cerr << "Error: Deserialized master key is incorrect" << endl;
-      exit(1);
-    }
-  }
-}
-
 static void BM_KPABE_DPVS_SerializeDecryptionKey(benchmark::State& state, policy_params params) {
   KPABE_DPVS kpabe;
   if (!kpabe.setup()) {
@@ -98,11 +28,35 @@ static void BM_KPABE_DPVS_SerializeDecryptionKey(benchmark::State& state, policy
   }
 }
 
+static void BM_KPABE_DPVS_DeserializeDecryptionKey(benchmark::State& state, policy_params params) {
+  KPABE_DPVS kpabe;
+  if (!kpabe.setup()) {
+    cerr << "Error: Could not setup KPABE_DPVS" << endl;
+    exit(1);
+  }
 
-// BENCHMARK(BM_KPABE_DPVS_SerializePublicKey)->Unit(benchmark::kMicrosecond);
-// BENCHMARK(BM_KPABE_DPVS_SerializeMasterKey)->Unit(benchmark::kMicrosecond);
-// BENCHMARK(BM_KPABE_DPVS_DeserializePublicKey)->Unit(benchmark::kMicrosecond);
-// BENCHMARK(BM_KPABE_DPVS_DeserializeMasterKey)->Unit(benchmark::kMicrosecond);
+  auto wl = generateAttributesList("WL_url_", params.nwl);
+  auto bl = generateAttributesList("BL_url_", params.nbl);
+  auto dec_key = kpabe.keygen(params.policy, wl, bl);
+
+  if (!dec_key) {
+    cerr << "Error: Could not generate keys" << endl;
+    exit(1);
+  }
+
+  OpenABEByteString dec_key_bytes;
+  dec_key->serialize(dec_key_bytes);
+  for (auto _ : state) {
+    KPABE_DPVS_DECRYPTION_KEY dec_key_deserialized;
+    dec_key_deserialized.deserialize(dec_key_bytes);
+
+    // Check if deserialized decryption key is correct
+    // if (!(dec_key_deserialized == *dec_key)) {
+    //   cerr << "Error: Deserialized decryption key is incorrect" << endl;
+    //   exit(1);
+    // }
+  }
+}
 
 
 string policy_1 = "Attr_5 and ((Attr_1 and Attr_2) or (Attr_3 and Attr_4))";
@@ -114,13 +68,6 @@ string policy_4 = "(Attr_5 and (Attr_1 or Attr_2)) and ((Attr_3 and Attr_4) or (
 // BENCHMARK_CAPTURE(BM_KPABE_DPVS_SerializeDecryptionKey, WL_10__BL_10__POLICY_2, 10, 10, policy_2)->Unit(benchmark::kMicrosecond);
 // BENCHMARK_CAPTURE(BM_KPABE_DPVS_SerializeDecryptionKey, WL_10__BL_10__POLICY_3, 10, 10, policy_3)->Unit(benchmark::kMicrosecond);
 // BENCHMARK_CAPTURE(BM_KPABE_DPVS_SerializeDecryptionKey, WL_10__BL_10__POLICY_4, 10, 10, policy_4)->Unit(benchmark::kMicrosecond);
-
-// BENCHMARK_CAPTURE(BM_KPABE_DPVS_SerializeDecryptionKey, WL_10__BL_10__POLICY_4,  10, 10,  policy_4)->Unit(benchmark::kMicrosecond);
-// BENCHMARK_CAPTURE(BM_KPABE_DPVS_SerializeDecryptionKey, WL_20__BL_10__POLICY_4,  20, 10,  policy_4)->Unit(benchmark::kMicrosecond);
-// BENCHMARK_CAPTURE(BM_KPABE_DPVS_SerializeDecryptionKey, WL_30__BL_10__POLICY_4,  30, 10,  policy_4)->Unit(benchmark::kMicrosecond);
-
-// BENCHMARK_CAPTURE(BM_KPABE_DPVS_SerializeDecryptionKey, WL_10__BL_100__POLICY_4,  10,  100, policy_4)->Unit(benchmark::kMicrosecond);
-// BENCHMARK_CAPTURE(BM_KPABE_DPVS_SerializeDecryptionKey, WL_100__BL_100__POLICY_4, 100, 100, policy_4)->Unit(benchmark::kMicrosecond);
 
 // Custom reporter to save results to CSV
 class CSVReporter : public benchmark::ConsoleReporter {
@@ -159,20 +106,52 @@ int main(int argc, char** argv) {
   // InitializeOpenABE();
   if (!init_libraries()) return 1;
 
-  for (int nwl = 0; nwl <= 100; nwl += 10) {
-    for (int nbl = 0; nbl <= 100; nbl += 10) {
-      std::string name = "WL_" + std::to_string(nwl) + "__BL_" + std::to_string(nbl) + "__POLICY_4";
-      policy_params params = {nwl, nbl, "policy_4"};
-      benchmark::RegisterBenchmark(name.c_str(), [params](benchmark::State& state) {
-        BM_KPABE_DPVS_SerializeDecryptionKey(state, params);
-      })->Unit(benchmark::kMicrosecond);
+  int __nwl = 10, __nbl = 10;
+  std::string filename = "benchmark--decryption-key--serialize.csv";
+
+  if (argc == 3) {
+    __nwl = std::stoi(argv[1]);
+    __nbl = __nwl;
+
+    if (std::string(argv[2]) == "serialize") {
+      for (int nwl = 0; nwl <= __nwl; nwl += 10) {
+        for (int nbl = 0; nbl <= __nwl; nbl += 10) {
+          std::string name = "WL_" + std::to_string(nwl) + "__BL_" + std::to_string(nbl) + "__POLICY_10";
+          policy_params params = {nwl, nbl, policy_4};
+          benchmark::RegisterBenchmark(name.c_str(), [params](benchmark::State& state) {
+            BM_KPABE_DPVS_SerializeDecryptionKey(state, params);
+          })->Unit(benchmark::kMicrosecond);
+        }
+      }
     }
+    else if (std::string(argv[2]) == "deserialize") {
+      for (int nwl = 0; nwl <= __nwl; nwl += 10) {
+        for (int nbl = 0; nbl <= __nwl; nbl += 10) {
+          std::string name = "WL_" + std::to_string(nwl) + "__BL_" + std::to_string(nbl) + "__POLICY_10";
+          policy_params params = {nwl, nbl, policy_4};
+          benchmark::RegisterBenchmark(name.c_str(), [params](benchmark::State& state) {
+            BM_KPABE_DPVS_DeserializeDecryptionKey(state, params);
+          })->Unit(benchmark::kMicrosecond);
+        }
+      }
+    }
+    else {
+      std::cerr << "Usage: " << argv[0] << " [number_attributes_WL_BL] [serialize|deserialize]" << std::endl;
+      return 1;
+    }
+    filename = "benchmark--decryption-key--" + std::string(argv[2]) + "--" + std::to_string(__nwl) + ".csv";
   }
+  else {
+    std::cerr << "Usage: " << argv[0] << " [number_attributes_WL_BL] [serialize|deserialize]" << std::endl;
+    return 1;
+  }
+
 
   // Run benchmark
   ::benchmark::Initialize(&argc, argv);
-  CSVReporter csv_reporter("benchmark_results.csv");
-  ::benchmark::RunSpecifiedBenchmarks(&csv_reporter);
+  // CSVReporter csv_reporter(filename);
+  // ::benchmark::RunSpecifiedBenchmarks(&csv_reporter);
+  ::benchmark::RunSpecifiedBenchmarks();
 
   // ShutdownOpenABE();
   clean_libraries();
