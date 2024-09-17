125ed1e756815ce7c84e91ce95aed2846c20b4d3
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2024-09-17 10:16:55 +0200

    Benchmak randomize public key method

--- bench/bench--setup--efficiency.cpp
+++ bench/bench--setup--efficiency.cpp
@@ -12,8 +12,40 @@ static void BM_KPABE_DPVS_Setup(benchmark::State& state) {
     }
   }
 }
-BENCHMARK(BM_KPABE_DPVS_Setup)->Unit(benchmark::kMillisecond);
 
+static void BM_KPABE_DPVS_PublicKeyRandomization(benchmark::State& state) {
+  KPABE_DPVS kpabe;
+  if (!kpabe.setup()) {
+    cerr << "Error: Could not setup KPABE_DPVS" << endl;
+    exit(1);
+  }
+
+  auto pk = kpabe.get_public_key();
+  for (auto _ : state) {
+    auto __pair = pk.randomize();
+  }
+}
+
+static void BM_KPABE_DPVS_ValidateDerivedPublicKey(benchmark::State& state) {
+  KPABE_DPVS kpabe;
+  if (!kpabe.setup()) {
+    cerr << "Error: Could not setup KPABE_DPVS" << endl;
+    exit(1);
+  }
+
+  auto pk = kpabe.get_public_key();
+  auto __pair = pk.randomize();
+  for (auto _ : state) {
+    if (!pk.validate_derived_key(__pair.first, __pair.second)) {
+      cerr << "Error: Could not validate public key" << endl;
+      exit(1);
+    }
+  }
+}
+
+BENCHMARK(BM_KPABE_DPVS_Setup)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_KPABE_DPVS_PublicKeyRandomization)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_KPABE_DPVS_ValidateDerivedPublicKey)->Unit(benchmark::kMillisecond);
 
 int main(int argc, char** argv) {
 
